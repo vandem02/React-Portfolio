bddb1bd8309b80064f0f07136dd77dae3fd904b1
Evan Vandenberg <vandem02@uwgb.edu>
2023-11-18 23:45:54 -0600

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 const Footer = () => {
   return (
-    <>
-      <h2>Footer</h2>
-    </>
+    <div className="mx-auto row mt-5" style={{ width: 400 }}>
+      <p className="col-12 text-center">&copy; Evan Vandenberg - {new Date().getFullYear()}</p>
+      <a className="col-4" href="https://github.com/vandem02" target="_blank">
+        <img alt="Github Badge" src="https://img.shields.io/badge/Github-%23181717?style=for-the-badge&logo=github"></img>
+      </a>
+      <a className="col-4" href="https://www.linkedin.com/in/evan-vandenberg-0565422a0">
+        <img alt="LinkedIn Badge" src="https://img.shields.io/badge/LinkedIn-%230A66C2?style=for-the-badge&logo=linkedin"></img>
+      </a>
+      <a className="col-4" href="https://www.facebook.com/Tronsnake">
+        <img alt="Facebook Badge" src="https://img.shields.io/badge/facebook-%230866FF?style=for-the-badge&logo=facebook"></img>
+      </a>
+    </div>
   );
 };
 
