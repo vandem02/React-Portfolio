fc423f88f3d56800c0a6b98015368cae4d433acf
Evan Vandenberg <vandem02@uwgb.edu>
2023-11-18 21:49:09 -0600

    added resume link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,3 +1,7 @@
-const Resume = () => <h1>Resume</h1>
+const Resume = () => {
+  return (
+    <p className="text-center m-3">You can download a copy of my resume <a href="../../images/Resume.pdf" download>here.</a></p>
+  )
+}
 
 export default Resume;
